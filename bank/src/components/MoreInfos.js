b2cc072f8db353a3714a3008ee74b21ec7f71425
Tawan Fonseca <webdevtawanfonseca@gmail.com>
2023-01-15 16:16:57 -0300

    Search areas done, starting creating footer section

---
+++ bank/src/components/MoreInfos.js
@@ -0,0 +1,18 @@
+import React from "react";
+import '../App.css';
+
+function MoreInfos(){
+    return(
+        <footer className="sectionContainer">
+            <h1> Mais informações</h1>
+            <div>
+                <div></div>
+                <div></div>
+            </div>
+        </footer>
+
+    )
+
+}
+
+export default MoreInfos
